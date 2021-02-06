510801cea410d988c384aee2ab929dadc8248a3e
steven poveda <HugoStevenPoveda@hotmail.com>
2021-02-06 13:11:02 -0500

    aprendiendo json

---
+++ index.js
@@ -0,0 +1,9 @@
+/* const express =require('express');
+const server = express();
+server.listen(3000,(req,res)=>{
+     
+    
+    
+    console.log("Server on port 3000")}) */
+    const server=require('live-server');
+    server.start()
---
+++ index1.js
@@ -0,0 +1,7 @@
+ const express =require('express');
+const server = express();
+server.listen(3000,(req,res)=>{
+     
+    
+    
+    console.log("Server on port 3000")}) 
